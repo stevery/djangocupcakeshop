6329ea64a9cb156654ef5bf626e4a6a336e1835d
stevery <triptokyw@gmail.com>
2016-08-15 18:02:11 +0900

    finished tutorial until Step 10

--- djangocupcakeshop/settings.py
+++ djangocupcakeshop/settings.py
@@ -20,12 +20,27 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1dp)-1!_ghogb^g-pqg@m25&k&fjemp*#c20-wc*)oaz8t@6b5'
+#SECRET_KEY = '1dp)-1!_ghogb^g-pqg@m25&k&fjemp*#c20-wc*)oaz8t@6b5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+
+DEBUG = os.getenv('DJANGO_DEBUG') != 'FALSE'
+
+# SECURITY WARNING: keep the secret key used in production secret!
+if DEBUG:
+    SECRET_KEY = 'Hell@World!'
+else:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+
+if DEBUG:
+    ALLOWED_HOSTS = ['*']
+else:
+    ALLOWED_HOSTS = ['djangocupcakeshop.pythonanywhere.com']
 
 
 # Application definition
--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from .models import Cupcake
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from django.shortcuts import redirect
+from .forms import CupcakeForm
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+
+
+def cupcake_list(request):
+    cakes = Cupcake.objects.all().order_by('-createAt')
+    context = {"cakes": cakes}
+    return render(request,"menu/list.html",context)
+
+
+def cupcake_detail(request,pk):
+    cake = get_object_or_404(Cupcake,pk=pk)
+    context = {"cake": cake}
+    return render(request,"menu/detail.html",context)
+
+
+@login_required
+def cupcake_new(request):
+    if request.method == "POST":
+        form = CupcakeForm(request.POST,request.FILES)
+        if form.is_valid():
+            cake = form.save(commit=False)
+            cake.createdAt = timezone.now()
+            cake.writer = request.user
+            cake.save()
+            return redirect('cupcake_detail',pk=cake.pk)
+    else:
+        form = CupcakeForm()
+    context = {'form':form}
+    return render(request,"menu/cupcake_new.html",context)
